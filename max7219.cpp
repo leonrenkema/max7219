d5b8026c4320bac7b0aec24bbf01199f42f8318d
Leon Renkema <leon@medimo.nl>
2021-01-13 21:34:40 +0100

    Some refactor

--- max7219.cpp
+++ max7219.cpp
@@ -59,9 +59,26 @@
 
 MAX7219:: MAX7219(void)
 {
-  pinMode(MAX_DIN, OUTPUT);
-  pinMode(MAX_CS, OUTPUT);
-  pinMode(MAX_CLK, OUTPUT);
+  pinDin = 12;
+  pinClk = 10;
+  pinCs = 11;
+
+  initPorts();
+}
+
+MAX7219::MAX7219(int din, int cs, int clk )
+{
+  pinDin = din;
+  pinCs = cs;
+  pinClk = clk;
+
+  initPorts();
+}
+
+void MAX7219::initPorts() {
+  pinMode(pinDin, OUTPUT);
+  pinMode(pinCs, OUTPUT);
+  pinMode(pinClk, OUTPUT);
 }
 
 void MAX7219::MAX7219_ShutdownStart (void)
@@ -76,8 +93,8 @@ void MAX7219:: MAX7219_DisplayTestStart (void)
 
 void MAX7219::Clear(void) {
     
-    for(int i=0;i<8;i++) {
-        MAX7219_Write(i+1,0x00);
+    for(uint8_t i=1;i<9;i++) {
+        MAX7219_Write(i,0x00);
     }
 }
 
@@ -86,16 +103,19 @@ void MAX7219::MAX7219_DisplayTestStop (void)
   MAX7219_Write(REG_DISPLAY_TEST, 0);                 
 }
 
-void MAX7219::MAX7219_SetBrightness (char brightness)
-{
-  brightness &= 0x0f;                                 
+void MAX7219::setBrightness (char brightness)
+{                             
   MAX7219_Write(REG_INTENSITY, brightness);           
 }
 
-unsigned char MAX7219::MAX7219_LookupCode (char character, unsigned int dp)
+/**
+ * @param char 
+ * @param bool Decimal point
+ */
+unsigned char MAX7219::MAX7219_LookupCode (char character, bool dp)
 {
-  int i;
-  unsigned int d=0;
+  uint8_t i;
+  uint8_t d=0;
  if (dp) d=1;  
   if (character>=35 && character<=44) {
     character+=13;
@@ -116,43 +136,45 @@ unsigned char MAX7219::MAX7219_LookupCode (char character, unsigned int dp)
   return 0;                                             
 }
 
-void MAX7219::DisplayText(char *text, int justify){
-  int decimal[16];
+void MAX7219::DisplayText(char *text, bool justify)
+{
+  bool decimal[16];
   char trimStr[16] = "";
-  int x,y=0;
-  int s;
+  uint8_t x,y=0;
+  uint8_t s;
   
   s=strlen(text);
   if (s>16) s=16;
   for (x=0;x<s;x++){
     if (text[x]=='.'){
-      decimal[y-1]=1;
+      decimal[y-1]=true;
       }
     else{
       trimStr[y]=text[x];
-      decimal[y]=0;
+      decimal[y]=false;
       y++;
      }
   }
   if (y>8) y=8;
-  for (x=0;x<y;x++){
-      if (justify==0)
-        DisplayChar((int)(y-x+7-y),trimStr[x], decimal[x]);    
-      if (justify==1){
-        DisplayChar((int)(y-x+7-y-(8-y)),trimStr[x], decimal[x]);    
-        
+  for (x=0;x<y;x++) {
+      if (justify) {
+        DisplayChar((uint8_t)(y-x+7-y),trimStr[x], decimal[x]);    
+      } else {
+        DisplayChar((uint8_t)(y-x+7-y-(8-y)),trimStr[x], decimal[x]);        
       }
   }
 }
 
-void MAX7219::MAX7219_Write(volatile byte opcode, volatile byte data) {
-    digitalWrite(MAX_CS,LOW);
-    shiftOut(MAX_DIN,MAX_CLK,MSBFIRST,opcode);
-    shiftOut(MAX_DIN,MAX_CLK,MSBFIRST,data);
-    digitalWrite(MAX_CS,HIGH);
+void MAX7219::MAX7219_Write(volatile byte opcode, volatile byte data) 
+{
+  digitalWrite(pinCs, LOW);
+  shiftOut(pinDin, pinClk, MSBFIRST, opcode);
+  shiftOut(pinDin, pinClk, MSBFIRST, data);
+  digitalWrite(pinCs,HIGH);
 }    
 
-void MAX7219::DisplayChar(int digit, char value, bool dp) {
+void MAX7219::DisplayChar(uint8_t digit, char value, bool dp) 
+{
       MAX7219_Write(digit+1,MAX7219_LookupCode(value, dp));
 }
 
@@ -163,11 +185,11 @@ void MAX7219::MAX7219_ShutdownStop (void)
  
 void MAX7219::Begin()
 { 
-  digitalWrite(MAX_CS,HIGH);
-  MAX7219_Write(REG_SCAN_LIMIT, 7);                   
+  digitalWrite(pinCs,HIGH);
+  MAX7219_Write(REG_SCAN_LIMIT, 0x07);                   
   MAX7219_Write(REG_DECODE, 0x00);                    
   MAX7219_ShutdownStop();                             
   MAX7219_DisplayTestStop();                          
   Clear();                                    
-  MAX7219_SetBrightness(INTENSITY_MAX);               
+  setBrightness(0x04);               
 }
--- max7219.h
+++ max7219.h
@@ -40,16 +40,17 @@
 
 //MAX7219
 #define REG_DECODE        0x09                        
-#define REG_INTENSITY     0x0a                       
+#define REG_INTENSITY     0x0a
+
+/**
+ * 
+ */
 #define REG_SCAN_LIMIT    0x0b                       
 #define REG_SHUTDOWN      0x0c                      
 #define REG_DISPLAY_TEST  0x0f                       
 
 #define INTENSITY_MIN     0x00                       
-#define INTENSITY_MAX     0x0f                      
-#define MAX_CLK 10
-#define MAX_CS 11
-#define MAX_DIN 12
+#define INTENSITY_MAX     0x0f  
 
 /*
 *********************************************************************************************************
@@ -83,7 +84,9 @@
 static const struct {
 	char   ascii;
 	char   segs;
-} MAX7219_Font[] = {
+} 
+
+MAX7219_Font[] = {
   {'A',0b1110111},{'B',0b1111111},{'C',0b1001110},{'D',0b1111110},{'E',0b1001111},{'F',0b1000111},       
   {'G',0b1011110},{'H',0b0110111},{'I',0b0110000},{'J',0b0111100},{'L',0b0001110},{'N',0b1110110},       
   {'O',0b1111110},{'P',0b1100111},{'R',0b0000101},{'S',0b1011011},{'T',0b0001111},{'U',0b0111110},       
@@ -92,29 +95,33 @@ static const struct {
   {'i',0b0010000},{'j',0b0111100},{'l',0b0001110},{'n',0b0010101},{'o',0b1111110},{'p',0b1100111},       
   {'r',0b0000101},{'s',0b1011011},{'t',0b0001111},{'u',0b0011100},{'y',0b0100111},{'-',0b0000001},
   {' ',0b0000000},{'0',0b1111110},{'1',0b0110000},{'2',0b1101101},{'3',0b1111001},{'4',0b0110011},
-  {'5',0b1011011},{'6',0b1011111},{'7',0b1110000},{'8',0b1111111},{'9',0b1111011},{'\0',0b0000000},
-  };
+  {'5',0b1011011},{'6',0b1011111},{'7',0b1110000},{'8',0b1111111},{'9',0b1111011},{'0',0b0000000},
+};
 
 class MAX7219
 {
-private:
+  private:
+    int pinCs, pinDin, pinClk;
 
-public: 
-  MAX7219(void);
-  void Begin(void);
-  void DisplayChar (char digit, char character, unsigned int dp);  
-  void MAX7219_DisplayTestStart (void);
-  void DisplayChar(int digit, char value, bool dp);
-  void clearDisplay();
-  void MAX7219_Write(volatile byte opcode, volatile byte data);
-  void MAX7219_ShutdownStop (void);
-  unsigned char MAX7219_LookupCode (char character, unsigned int dp);
-  void MAX7219_ShutdownStart (void);
-  void Clear (void);
-  void MAX7219_DisplayTestStop (void);
-  void MAX7219_SetBrightness (char brightness);
-  void DisplayText(char *text, int justify);
+    void initPorts();
+    unsigned char MAX7219_LookupCode (char, bool);
 
+  public: 
+    MAX7219(void);
+    MAX7219(int, int, int);
+
+    void Begin(void);
+    void DisplayChar(uint8_t, char, bool);
+
+    void MAX7219_DisplayTestStart (void);
+    void clearDisplay();
+    void MAX7219_Write(volatile byte, volatile byte);
+    void MAX7219_ShutdownStop (void);
+    void MAX7219_ShutdownStart (void);
+    void Clear (void);
+    void MAX7219_DisplayTestStop (void);
+    void setBrightness (char brightness);
+    void DisplayText(char *text, bool justify);
 };
 
 #endif
